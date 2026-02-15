bbb79f41e1b773771b2d81073490106e0fc8ddbd
Ankullll <ankulsingh975@gmail.com>
2026-02-15 16:00:39 +0530

    Completed Project

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -6,94 +6,112 @@ import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 
 const Create = () => {
-    const {recipe,setrecipe} = useContext(recipeContext)
-    const navigate = useNavigate()
-     
-  const {
-    register,
-    handleSubmit,
-    reset,
-  } = useForm();
+  const { recipe, setrecipe } = useContext(recipeContext);
+  const navigate = useNavigate();
+
+  const { register, handleSubmit, reset } = useForm();
   const submitHandler = (data) => {
     data.id = nanoid();
-    const copydata = [...recipe]
-    copydata.push(data)
-    setrecipe(copydata)
-    localStorage.setItem("recipes",JSON.stringify(copydata))
-   
-    toast.success("New Recipe Added !")
-    navigate("/recipes")
+    const copydata = [...recipe];
+    copydata.push(data);
+    setrecipe(copydata);
+    localStorage.setItem("recipes", JSON.stringify(copydata));
+
+    toast.success("New Recipe Added !");
+    navigate("/recipes");
     reset();
   };
-   
- 
+
   return (
     <div>
-      <img src="/assets/flower-japan.svg" alt="" className="h-[5.5vh] w-[4.5vw] absolute top-34 left-60 " />
-      <img src="/assets/flower-japan.svg" alt="" className="h-[5.5vh] w-[4.5vw] absolute top-134 left-20 " />
-      <img src="/assets/flower-japan.svg" alt="" className="h-[5.5vh] w-[4.5vw] absolute top-94 left-280 " />
-     
-      <img src="/assets/leaf-1.svg" alt="" className="h-[5.5vh] w-[4.5vw] absolute top-100 left-90 " />
-      <img src="/assets/leaf-2.svg" alt="" className="h-[5.5vh] w-[4.5vw] absolute top-150 left-220 " />
-      <img src="/assets/leaf-3.svg" alt="" className="h-[5.5vh] w-[4.5vw] absolute top-35 left-150 " />
+      <img
+        src="/assets/flower-japan.svg"
+        alt=""
+        className="h-[5.5vh] w-[4.5vw] absolute top-34 left-60 "
+      />
+      <img
+        src="/assets/flower-japan.svg"
+        alt=""
+        className="h-[5.5vh] w-[4.5vw] absolute top-134 left-20 "
+      />
+      <img
+        src="/assets/flower-japan.svg"
+        alt=""
+        className="h-[5.5vh] w-[4.5vw] absolute top-94 left-280 "
+      />
+
+      <img
+        src="/assets/leaf-1.svg"
+        alt=""
+        className="h-[5.5vh] w-[4.5vw] absolute top-100 left-90 "
+      />
+      <img
+        src="/assets/leaf-2.svg"
+        alt=""
+        className="h-[5.5vh] w-[4.5vw] absolute top-150 left-220 "
+      />
+      <img
+        src="/assets/leaf-3.svg"
+        alt=""
+        className="h-[5.5vh] w-[4.5vw] absolute top-35 left-150 "
+      />
       <div className="bg-[#be3740] w-[55%] mx-auto mt-20 rounded-2xl overflow-hidden">
-      
-      <h1 className="font-[Lora] text-3xl text-center mt-5 text-[#feefdf] -mb-1 ">Create New Recipe </h1>
-      <form onSubmit={handleSubmit(submitHandler)}>
-      <input
-        className="block outline-0 bg-[#fcbfbc] border border-[#494139]  text-sm   w-[70%] px-3 py-2.5 shadow-xs  mx-auto mt-7 rounded-2xl mb-2 text-center"
-        type="url"
-        placeholder="Enter image url"
-        {...register("image")}
-      />
-      {/* <small className="text-red-500 p-2">This is how the error is shown</small> */}
-      <input
-        {...register("title")}
-        type="text"
-        placeholder="Recipe Title"
-        className="block outline-0 bg-[#fcbfbc] border border-[#494139]  text-sm   w-[70%] px-3 py-2.5 shadow-xs  mx-auto mt-4 rounded-2xl mb-2 text-center"
-      />
-      <input
-        {...register("chef")}
-        type="text"
-        placeholder="Chef Name"
-        className="block outline-0 bg-[#fcbfbc] border border-[#494139]  text-sm   w-[70%] px-3 py-2.5 shadow-xs  mx-auto mt-4 rounded-2xl mb-2 text-center"
-      />
-      {/* <small className="text-red-500 p-2">This is how the error is shown</small> */}
-      <textarea
-        {...register("description")}
-        type="text"
-        placeholder="Description"
-        className="block outline-0 bg-[#fcbfbc] border border-[#494139]  text-sm   w-[70%] px-3 py-2.5 shadow-xs  mx-auto mt-4 rounded-2xl mb- text-center"
-      />
-      {/* <small className="text-red-500 p-2">This is how the error is shown</small> */}
-      <textarea
-        {...register("ingredients")}
-        type="text"
-        placeholder="Write ingredients separated by comma"
-        className="block outline-0 bg-[#fcbfbc] border border-[#494139]  text-sm   w-[70%] px-3 py-2.5 shadow-xs  mx-auto mt-4 rounded-2xl mb-2 text-center"
-      />
-      {/* <small className="text-red-500 p-2">This is how the error is shown</small> */}
-     
-      <select
-        {...register("category")}
-        type="text"
-        className="block outline-0 bg-[#fcbfbc] border border-[#494139]  text-sm   w-[70%] px-3 py-2.5 shadow-xs  mx-auto mt-4 rounded-2xl mb-2  text-center"
-      >
-        <option value="breakfast">Breakfast</option>
-        <option value="lunch">Lunch</option>
-        <option value="supper">Supper</option>
-        <option value="dinner">Dinner</option>
-      </select>
-      {/* <small className="text-red-500 p-2">This is how the error is shown</small> */}
-      <button className="block outline-0 bg-[#23bc33] border border-[#494139]  text-sm   w-[40%] px-3 py-2.5 shadow-xs  mx-auto mt-4 rounded-2xl mb-5">
-        Save Recipe
-      </button>
-    </form>
+        <h1 className="font-[Lora] text-3xl text-center mt-5 text-[#feefdf] -mb-1 ">
+          Create New Recipe{" "}
+        </h1>
+        <form onSubmit={handleSubmit(submitHandler)}>
+          <input
+            className="block outline-0 bg-[#fcbfbc] border border-[#494139]  text-sm   w-[70%] px-3 py-2.5 shadow-xs  mx-auto mt-7 rounded-2xl mb-2 text-center"
+            type="url"
+            placeholder="Enter image url"
+            {...register("image")}
+          />
+          {/* <small className="text-red-500 p-2">This is how the error is shown</small> */}
+          <input
+            {...register("title")}
+            type="text"
+            placeholder="Recipe Title"
+            className="block outline-0 bg-[#fcbfbc] border border-[#494139]  text-sm   w-[70%] px-3 py-2.5 shadow-xs  mx-auto mt-4 rounded-2xl mb-2 text-center"
+          />
+          <input
+            {...register("chef")}
+            type="text"
+            placeholder="Chef Name"
+            className="block outline-0 bg-[#fcbfbc] border border-[#494139]  text-sm   w-[70%] px-3 py-2.5 shadow-xs  mx-auto mt-4 rounded-2xl mb-2 text-center"
+          />
+          {/* <small className="text-red-500 p-2">This is how the error is shown</small> */}
+          <textarea
+            {...register("description")}
+            type="text"
+            placeholder="Description"
+            className="block outline-0 bg-[#fcbfbc] border border-[#494139]  text-sm   w-[70%] px-3 py-2.5 shadow-xs  mx-auto mt-4 rounded-2xl mb- text-center"
+          />
+          {/* <small className="text-red-500 p-2">This is how the error is shown</small> */}
+          <textarea
+            {...register("ingredients")}
+            type="text"
+            placeholder="Write ingredients separated by comma"
+            className="block outline-0 bg-[#fcbfbc] border border-[#494139]  text-sm   w-[70%] px-3 py-2.5 shadow-xs  mx-auto mt-4 rounded-2xl mb-2 text-center"
+          />
+          {/* <small className="text-red-500 p-2">This is how the error is shown</small> */}
+
+          <select
+            {...register("category")}
+            type="text"
+            className="block outline-0 bg-[#fcbfbc] border border-[#494139]  text-sm   w-[70%] px-3 py-2.5 shadow-xs  mx-auto mt-4 rounded-2xl mb-2  text-center"
+          >
+            <option value="breakfast">Breakfast</option>
+            <option value="lunch">Lunch</option>
+            <option value="supper">Supper</option>
+            <option value="dinner">Dinner</option>
+          </select>
+          {/* <small className="text-red-500 p-2">This is how the error is shown</small> */}
+          <button className="block outline-0 bg-[#23bc33] border border-[#494139]  text-sm   w-[40%] px-3 py-2.5 shadow-xs  mx-auto mt-4 rounded-2xl mb-5">
+            Save Recipe
+          </button>
+        </form>
+      </div>
     </div>
-    </div>
-    
-    
   );
 };
 
--- src/pages/SingleRecipe.jsx
+++ src/pages/SingleRecipe.jsx
@@ -1,4 +1,4 @@
-import { useContext,  useState } from "react";
+import { useContext, useState } from "react";
 
 import { recipeContext } from "../context/RecipeContext";
 import { useForm } from "react-hook-form";
@@ -11,17 +11,14 @@ const SingleRecipe = () => {
   const { recipe, setrecipe } = useContext(recipeContext);
   const data = recipe.find((e) => params.id == e.id);
 
-  const { register, handleSubmit} = useForm({
-    defaultValues:{
-        title : data?.title,
-        image : data?.image,
-        chef : data?.chef,
-        description: data?.description,
-        ingredients : data?.ingredients,
-
-
-
-    }
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      title: data?.title,
+      image: data?.image,
+      chef: data?.chef,
+      description: data?.description,
+      ingredients: data?.ingredients,
+    },
   });
 
   const updateHandler = (data) => {
@@ -29,10 +26,9 @@ const SingleRecipe = () => {
     const copyrecipe = [...recipe];
     copyrecipe[index] = { ...copyrecipe[index], ...data };
     setrecipe(copyrecipe);
-    localStorage.setItem("recipes",JSON.stringify(copyrecipe))
-    toast.success("Recipe Updated")
-    navigate("/recipes")
-   
+    localStorage.setItem("recipes", JSON.stringify(copyrecipe));
+    toast.success("Recipe Updated");
+    navigate("/recipes");
   };
   const deleteHandler = () => {
     let filter = recipe.filter((e) => e.id != params.id);
@@ -43,105 +39,124 @@ const SingleRecipe = () => {
     localStorage.setItem("fav", JSON.stringify(updatedFav));
     navigate("/recipes");
     toast.error("Recipe deleted");
-    localStorage.setItem("recipes",JSON.stringify(filter))
+    localStorage.setItem("recipes", JSON.stringify(filter));
   };
 
-  const [favdata, setfavdata] = useState(JSON.parse(localStorage.getItem("fav")) || [])
+  const [favdata, setfavdata] = useState(
+    JSON.parse(localStorage.getItem("fav")) || [],
+  );
   //  useEffect(()=>{
-    
+
   //     return ()=>{
   //     }
   //   },[favdata])
 
-    
- 
-    const FavHandler = ()=>{
-      let copyfav =[...favdata]
-      copyfav.push(data)
-      setfavdata(copyfav)
-      localStorage.setItem("fav",JSON.stringify(copyfav))
-    }
-  
-    const UnFavHandler = ()=>{
-      const unlike = favdata.filter( e=> e.id != data?.id)
-      setfavdata(unlike)
-      localStorage.setItem("fav",JSON.stringify(unlike))
-      
-    }
+  const FavHandler = () => {
+    let copyfav = [...favdata];
+    copyfav.push(data);
+    setfavdata(copyfav);
+    localStorage.setItem("fav", JSON.stringify(copyfav));
+  };
+
+  const UnFavHandler = () => {
+    const unlike = favdata.filter((e) => e.id != data?.id);
+    setfavdata(unlike);
+    localStorage.setItem("fav", JSON.stringify(unlike));
+  };
   return recipe ? (
     <div className="w-full flex">
-      
       <div className="left  w-1/2 p-2">
-      {favdata?.find(f => f?.id == data?.id)?   <i onClick={UnFavHandler} className="absolute ri-heart-3-fill text-3xl text-[#be3740] left-[35%] tip-31"></i>:<i onClick={FavHandler} className="absolute ri-heart-add-line text-3xl text-[#be3740] left-[35%] top-31"></i>
-}
+        {favdata?.find((f) => f?.id == data?.id) ? (
+          <i
+            onClick={UnFavHandler}
+            className="absolute ri-heart-3-fill text-3xl text-[#be3740] left-[35%] tip-31"
+          ></i>
+        ) : (
+          <i
+            onClick={FavHandler}
+            className="absolute ri-heart-add-line text-3xl text-[#be3740] left-[35%] top-31"
+          ></i>
+        )}
 
-      
-     
-
-        <h1 className="text-3xl font-black w-[55%] mr-1 ">{data?.title}</h1>
-        <img className="h-[30vh] mt-5" src={data?.image} alt="" />
-        <h2 className="text-2xl font-black">{data?.chef}</h2>
+        <h1 className="text-3xl font-[Lora] font-bold w-[55%] mr-1 -mb-2 ">
+          {data?.title}
+        </h1>
+        <img
+          className="h-[30vh] mt-5 rounded-2xl -ml-3 mb-5 "
+          src={data?.image}
+          alt=""
+        />
+        <h2 className="text-xl text-[#be3740] font-[Lora]">{data?.chef}</h2>
+        <p className=" text-[#918273]  text-s w-[50%]">{data?.description}</p>
       </div>
-      <form className="w-1/2 " onSubmit={handleSubmit(updateHandler)}>
-        <input
-          className="block p-2 border-b outline-0"
-          type="url"
-          placeholder="Enter image url"
-          {...register("image")}
-        />
-        <small className="text-red-500 p-2">
-          This is how the error is shown
-        </small>
-        <input
-          {...register("title")}
-          type="text"
-          placeholder="Recipe Title"
-          className="block p-2 border-b outline-0"
-        />
-        <input
-          {...register("chef")}
-          type="text"
-          placeholder="Chef Name"
-          className="block p-2 border-b outline-0"
-        />
-        {/* <small className="text-red-500 p-2">This is how the error is shown</small> */}
-        <textarea
-          {...register("description")}
-          type="text"
-          placeholder="Description"
-          className="block p-2 border-b outline-0"
-        />
-        {/* <small className="text-red-500 p-2">This is how the error is shown</small> */}
-        <textarea
-          {...register("ingredients")}
-          type="text"
-          placeholder="Write ingredients separated by comma"
-          className="block p-2 border-b outline-0"
-        />
-        {/* <small className="text-red-500 p-2">This is how the error is shown</small> */}
 
-        <select
-          {...register("category")}
-          type="text"
-          className="block p-2 border-b outline-0 bg-gray-800"
-        >
-          <option value="breakfast">Breakfast</option>
-          <option value="lunch">Lunch</option>
-          <option value="supper">Supper</option>
-          <option value="dinner">Dinner</option>
-        </select>
-        {/* <small className="text-red-500 p-2">This is how the error is shown</small> */}
-        <button  className="mt-5 block py-2 px-4  rounded bg-blue-900">
-          Update Recipe
-        </button>
-        <button
-          onClick={deleteHandler}
-          className="mt-5 block py-2 px-4  rounded bg-red-900"
-          type="button"
-        >
-          Delete Recipe
-        </button>
-      </form>
+
+
+
+
+
+      <div className="bg-[#be3740] w-[45%]   rounded-2xl overflow-hidden">
+        <form className="w-1/2 " onSubmit={handleSubmit(updateHandler)}>
+          <input
+            className="block outline-0 bg-[#fcbfbc] border border-[#494139]  text-sm   w-[30vw] px-3 py-2.5 shadow-xs ml-10 mt-7 rounded-2xl  text-center"
+            type="url"
+            placeholder="Enter image url"
+            {...register("image")}
+          />
+          {/* <small className="text-red-500 p-2">
+            This is how the error is shown
+          </small> */}
+          <input
+            {...register("title")}
+            type="text"
+            placeholder="Recipe Title"
+            className="block outline-0 bg-[#fcbfbc] border border-[#494139]  text-sm   w-[30vw] px-3 py-2.5 shadow-xs ml-10 mt-2 rounded-2xl  text-center"
+          />
+          <input
+            {...register("chef")}
+            type="text"
+            placeholder="Chef Name"
+            className="block outline-0 bg-[#fcbfbc] border border-[#494139]  text-sm   w-[30vw] px-3 py-2.5 shadow-xs ml-10 mt-2 rounded-2xl  text-center"
+          />
+          {/* <small className="text-red-500 p-2">This is how the error is shown</small> */}
+          <textarea
+            {...register("description")}
+            type="text"
+            placeholder="Description"
+            className="block outline-0 bg-[#fcbfbc] border border-[#494139]  text-sm   w-[30vw] px-3 py-2.5 shadow-xs ml-10 mt-2 rounded-2xl  text-center"
+          />
+          {/* <small className="text-red-500 p-2">This is how the error is shown</small> */}
+          <textarea
+            {...register("ingredients")}
+            type="text"
+            placeholder="Write ingredients separated by comma"
+            className="block outline-0 bg-[#fcbfbc] border border-[#494139]  text-sm   w-[30vw] px-3 py-2.5 shadow-xs ml-10 mt-2 rounded-2xl  text-center"
+          />
+          {/* <small className="text-red-500 p-2">This is how the error is shown</small> */}
+
+          <select
+            {...register("category")}
+            type="text"
+            className="block outline-0 bg-[#fcbfbc] border border-[#494139]  text-sm   w-[30vw] px-3 py-2.5 shadow-xs ml-10 mt-2 rounded-2xl  text-center"
+          >
+            <option value="breakfast">Breakfast</option>
+            <option value="lunch">Lunch</option>
+            <option value="supper">Supper</option>
+            <option value="dinner">Dinner</option>
+          </select>
+          {/* <small className="text-red-500 p-2">This is how the error is shown</small> */}
+          <button className="block outline-0 bg-[#7edc75]  border border-[#494139]  text-sm   w-[20vw] px-3 py-2.5 shadow-xs ml-28 mt-2 rounded-2xl  text-center ">
+            Update Recipe
+          </button>
+          <button
+            onClick={deleteHandler}
+            className="block outline-0 bg-red-500 border border-[#494139]  text-sm   w-[20vw] px-3 py-2.5 shadow-xs ml-28 mt-2 rounded-2xl mb-3 text-center"
+            type="button"
+          >
+            Delete Recipe
+          </button>
+        </form>
+      </div>
     </div>
   ) : (
     "Loading"
